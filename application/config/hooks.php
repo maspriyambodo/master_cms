e4b63d64ffd393bb84448e007f26098014c96928
maspriyambodo <maspriyambodo@gmail.com>
2021-09-03 23:45:38 +0700

    fixed untracked files

--- application/config/hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-$hook['display_override'][] = array(
-    'class' => '',
-    'function' => 'compress',
-    'filename' => 'compress.php',
-    'filepath' => 'hooks'
-);
