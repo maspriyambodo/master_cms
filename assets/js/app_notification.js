49fb76c3bf594de7fc2cfc68dc3ed776ee11bd75
maspriyambodo <maspriyambodo@gmail.com>
2021-09-11 17:06:51 +0700

    add function mark all as read

--- assets/js/app_notification.js
+++ assets/js/app_notification.js
@@ -2,38 +2,33 @@ var pusher = new Pusher('4587e4cb86b14bb98e69', {
     cluster: 'ap1'
 });
 setInterval(function () {
-    if (pusher.connection.state === 'disconnected') {
-        console.log(pusher.connection.state);
-        pusher.connect();
-    } else {
-        console.log(pusher.connection.state);
-    }
-
-}, 60000);
+    pusher.connect();
+}, 5000);
 var channel = pusher.subscribe('app_notification-channel');
+var audio = {};
+audio["walk"] = new Audio();
+audio["walk"].src = "assets/media/sound/notification_sound.mp3";
 channel.bind('app_notification-event', function (data) {
+    $('#notif_count').show();
     $('#notif_count').text(data.tot_notif);
     $('#kt_quick_panel_logs').empty();
-    var audio = {};
-    audio["walk"] = new Audio();
-    audio["walk"].src = "assets/media/sound/notification_sound.mp3";
     audio["walk"].play();
 });
 function dir_notif() {
     $('#kt_quick_panel_logs').empty();
     $.ajax({
         url: "Notification/Get_notif",
-        cache: false,
-        contentType: false,
         processData: true,
         success: function (data) {
             if (data[0].title_notif === 'kosong') {
-                console.log('kosong');
+                $('#notif_count').hide();
             } else {
+                $('#notif_count').hide();
+                $('#kt_quick_panel_logs').append('<button id="btn_notif" type="button" class="btn btn-default btn-xs mb-4" onclick="Read_notif()">mark all as read</button>');
                 var i;
                 for (i = 0; i < data.length; i++) {
                     $('#kt_quick_panel_logs').append(
-                            '<div class="d-flex align-items-center flex-wrap mb-5">'
+                            '<div class="d-flex align-items-center flex-wrap mb-5 bg-light-primary py-4 px-4">'
                             + '<div class="d-flex flex-column flex-grow-1 mr-2">'
                             + '<a href="' + data[i].url_link + '" class="font-weight-bolder text-dark-75 text-hover-primary font-size-lg mb-1">'
                             + data[i].title_notif
@@ -50,5 +45,12 @@ function dir_notif() {
         error: function (jqXHR) {
 
         }
+    });
+}
+function Read_notif() {
+    $('#kt_quick_panel_logs').empty();
+    $.ajax({
+        url: "Notification/Mark_all_read",
+        processData: true
     });
 }
