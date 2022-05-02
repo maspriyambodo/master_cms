f927e109672a3edc6b2136bd08724253b347e0f3
maspriyambodo <maspriyambodo@gmail.com>
2022-05-03 00:34:05 +0700

    change assets to local

--- application/modules/Template/views/layout.php
+++ application/modules/Template/views/layout.php
@@ -31,8 +31,8 @@ $group_menu = $this->M_default->Group_menu();
         <link href="<?php echo base_url('assets/css/themes/layout/header/menu/dark.css'); ?>" rel="stylesheet" type="text/css" crossorigin="anonymous"/>
         <link href="<?php echo base_url('assets/css/themes/layout/brand/dark.css'); ?>" rel="stylesheet" type="text/css" crossorigin="anonymous"/>
         <link href="<?php echo base_url('assets/css/themes/layout/aside/dark.css'); ?>" rel="stylesheet" type="text/css" crossorigin="anonymous"/>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" rel="stylesheet"/>
-        <link href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.8/kt-2.6.1/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url('assets/@fortawesome/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url('assets/datatables/datatables.min.css'); ?>" rel="stylesheet" type="text/css"/>
     </head>
     <body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
         <div id="kt_header_mobile" class="header-mobile align-items-center header-mobile-fixed">
@@ -176,9 +176,10 @@ $group_menu = $this->M_default->Group_menu();
                         <script src="<?php echo base_url('assets/js/scripts.bundle.js'); ?>" type="text/javascript"></script>
                         <script src="<?php echo base_url('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js'); ?>" type="text/javascript"></script>
                         <script src="<?php echo base_url('assets/js/pages/widgets.js'); ?>" type="text/javascript"></script>
-                        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js" type="text/javascript"></script>
-                        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js" type="text/javascript"></script>
-                        <script src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.8/kt-2.6.1/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.js" type="text/javascript"></script>
+                        <script src="<?php echo base_url('assets/@fortawesome/fontawesome-free/js/all.min.js'); ?>" type="text/javascript"></script>
+                        <script src="<?php echo base_url('assets/datatables/pdfmake-0.1.36/pdfmake.min.js'); ?>" type="text/javascript"></script>
+                        <script src="<?php echo base_url('assets/datatables/pdfmake-0.1.36/vfs_fonts.js'); ?>" type="text/javascript"></script>
+                        <script src="<?php echo base_url('assets/datatables/datatables.min.js'); ?>" type="text/javascript"></script>
                         <!--<script src="https://js.pusher.com/7.0/pusher.min.js"></script>-->
                         <div class="d-flex flex-column-fluid"><div class="container-fluid">{content}</div></div>
                     </div>
