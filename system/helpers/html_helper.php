ec5d8292db0cc11f75e1b4b03351d074246ccf7f
maspriyambodo <maspriyambodo@gmail.com>
2022-05-26 14:25:39 +0700

    resolve error

--- system/helpers/html_helper.php
+++ system/helpers/html_helper.php
@@ -363,7 +363,7 @@ if ( ! function_exists('meta'))
 		$str = '';
 		foreach ($name as $meta)
 		{
-			$type		= (isset($meta['type']) && $meta['type'] !== 'name')	? 'http-equiv' : 'name';
+			$type		= (isset($meta['type']) && $meta['type'] !== 'name')	? $type : 'name';
 			$name		= isset($meta['name'])					? $meta['name'] : '';
 			$content	= isset($meta['content'])				? $meta['content'] : '';
 			$newline	= isset($meta['newline'])				? $meta['newline'] : "\n";
